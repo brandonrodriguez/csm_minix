75d87efc6c511aae2e8b68760262d648fc2ef8f8
Big Brother <root@10.0.2.15>
2013-10-22 06:20:46 +0000

    Ready for Student Distribution

--- OSPEX/procs.c
+++ OSPEX/procs.c
@@ -1,7 +1,5 @@
 #include "procs.h"
 
-/*Array for storing the proc numbers of forked processes. This is used to filter the procs shown by the GUI*/
-pid_t pid_array[PROCNUM];
 
 void procs(void){
 
@@ -24,7 +22,31 @@ printf("Hit Proc Process\n");
 				case(1):
 					execl("./proc2",NULL);
 					break;
-				}
+				case(2):
+					execl("./proc3",NULL);
+					break;
+				case(3):
+					execl("./proc4",NULL);
+					break;
+				case(4):
+					execl("./proc5",NULL);
+					break;
+				case(5):
+					execl("./proc6",NULL);
+					break;
+				case(6):
+					execl("./proc7",NULL);
+					break;
+				case(7):
+					execl("./proc8",NULL);
+					break;
+				case(8):
+					execl("./proc9",NULL);
+					break;
+				case(9):
+					execl("./proc10",NULL);
+					break;
+			}
 				break;
 		}	
 		else{
--- OSPEX/procs.h
+++ OSPEX/procs.h
@@ -6,6 +6,8 @@
 
 void procs(void);
 
-#define PROCNUM 2
+#define PROCNUM 10 
 extern pid_t pid_array[PROCNUM];
+/*Array for storing the proc numbers of forked processes. This is used to filter the procs shown by the GUI*/
+pid_t pid_array[PROCNUM];
 #endif
